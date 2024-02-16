f14261965a247e8d5ea1123e38c98afc9b544e4d
chushtarov <kenlol@bk.ru>
2024-02-16 16:50:07 +0300

    добавил фреймворк tailwind

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-        <div>Logo</div>
-        <div><Link></Link></div>
+    <div className="">
+      <div>Logo</div>
+      <div>
+        <ul>
+          <li><NavLink to="/">Объявление</NavLink></li>
+          <li><NavLink to="/">В добрые руки</NavLink></li>
+          <li><NavLink to="/">О нас</NavLink></li>
+          <li><NavLink to="/">О проекте</NavLink></li>
+        </ul>
+      </div>
+      <div>
+        <ul>
+          <li>Создать объявление</li>
+          <li>Войти</li>
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
