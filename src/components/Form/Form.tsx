855a310385d2e1b2290c9b56ed5247a698b9ec6a
chushtarov <kenlol@bk.ru>
2024-02-20 00:01:43 +0300

    докончил форму отправки, осталось доделать функционал

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -3,14 +3,14 @@ import img from "../../../public/subscribe.png";
 
 const Form = () => {
   return (
-    <div className="bg-[#a1e0ff] h-[340px]">
+    <div className="bg-[#00876d] h-[340px] mt-12">
       <div className="w-[1200px] m-auto flex justify-between items-center">
-        <div>
+        <div className="text-white">
           <h1 className="text-[45px]  my-5">Подпишитесь на новости</h1>
           <p className="my-5">И будьте в курсе всех событий из мира технологий</p>
-          <form action="">
-            <input type="email" name="" placeholder="Ваш email" id="" />
-            <button>Подписаться</button>
+          <form action="" className="flex">
+            <input className='px-5 text-black w-[473px] h-[50px] rounded-lg' type="email" name="" placeholder="Ваш email" id="" />
+            <button className="-ml-10 w-[173px] uppercase h-[50px] text-white bg-[#f9961e] rounded-full text-sm hover:bg-[#fff] hover:text-[#f9961e] hover:border-orange-400 hover:border">Подписаться</button>
           </form>
         </div>
         <div >
