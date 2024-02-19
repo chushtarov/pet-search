15057b2d9f882e7f176b5bc211e696af65a1c4c2
chushtarov <kenlol@bk.ru>
2024-02-19 23:51:48 +0300

    остались доработки стилей

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,18 +1,24 @@
-import React from 'react'
+import React from "react";
+import img from "../../../public/subscribe.png";
 
 const Form = () => {
   return (
-    <div className='bg-[#a1e0ff] h-[340px]'>
-        <div><h1>Подпишитесь на новости</h1>
-        <p>И будьте в курсе всех событий из мира технологий</p>
-        <form action="">
-            <input type="email" name="" placeholder='Ваш email' id="" />
+    <div className="bg-[#a1e0ff] h-[340px]">
+      <div className="w-[1200px] m-auto flex justify-between items-center">
+        <div>
+          <h1 className="text-[45px]  my-5">Подпишитесь на новости</h1>
+          <p className="my-5">И будьте в курсе всех событий из мира технологий</p>
+          <form action="">
+            <input type="email" name="" placeholder="Ваш email" id="" />
             <button>Подписаться</button>
-        </form>
+          </form>
         </div>
-        <div></div>
+        <div >
+          <img className="w-[335px]" src={img} alt="" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
