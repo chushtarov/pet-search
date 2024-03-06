8ecca8683c9e15b3906a34cc4dfab20a5bfade4d
chushtarov <kenlol@bk.ru>
2024-03-06 20:30:53 +0300

    сделал чуток верстку и доюавил фейк данные

--- src/components/Give/Give.tsx
+++ src/components/Give/Give.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { givePet } from "../../../public/facaBD/faceBD";
 
 const Give = () => {
   return (
-    <div>Give</div>
-  )
-}
+    <div className="w-[1200px] bg-slate-400 m-auto">
+      <h1>Хочешь себе питомца?</h1>
+      <ul className="flex flex-wrap justify-between p-2 w-[1200px]">
+        {givePet.map((item, i) => {
+          return (
+            <li key={i} className="w-[550px] cursor-pointer mb-2 flex bg-slate-100 ">
+              <span className="w-[300px] p-1">
+                <img src={item.img} alt="cat" />
+              </span>
+              <span>
+                <p>{item.title}</p>
+                <p>{item.description}</p>
+                <p>{item.map}</p>
+              </span>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 
-export default Give
+export default Give;
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,44 +1,56 @@
-import React, { ChangeEvent, FormEvent, useState } from 'react'
+import React, { ChangeEvent, FormEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-    const [login, setLogin] = useState("")
-    const [password, setPassword] = useState("")
-const hendleSignUp = (e: FormEvent) => {
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const hendleSignUp = (e: FormEvent) => {
     e.preventDefault();
-}
+  };
 
-    const handleLogin = (e: ChangeEvent<HTMLInputElement>) => {
-        setLogin(e.target.value)
-    }
+  const handleLogin = (e: ChangeEvent<HTMLInputElement>) => {
+    setLogin(e.target.value);
+  };
 
-    const handlePassword = (e: ChangeEvent<HTMLInputElement>) => {
-        setPassword(e.target.value)
-        console.log(login, password)
-    }
+  const handlePassword = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+    console.log(login, password);
+  };
+
+//   if (token) {
+//     navigate("/");
+//   }
+
   return (
-    <div className='bg-slate-100 w-[500px] m-auto mt-[210px] rounded-xl'>
-        <h1 className='text-center pt-10 text-[24px] uppercase'>Авторизация</h1>
-        <form 
-        className='flex flex-col' 
-        onSubmit={hendleSignUp}>
-            <input 
-            className='w-[260px] mt-10 m-auto  h-[40px] rounded-full pl-3 focus:outline-none focus:ring-1 focus:ring-orange-400'
-            placeholder='login'
-            onChange={handleLogin}
-            value={login}
-            type="text" />
-            <input 
-            className='w-[260px] mt-5 m-auto focus:outline-none focus:ring-1 focus:ring-orange-400 h-[40px] rounded-full pl-3'
-            placeholder='passsword'
-            onChange={handlePassword}
-            value={password}
-            type="password" />
-            <button 
-            className=" w-[173px] uppercase h-[40px] text-[#f9961e] m-auto my-10 bg-[#fff] rounded-full text-sm hover:bg-[#f9961e] hover:text-white hover:border-orange-400 hover:border border border-orange-400"
-            type='submit'>Войти</button>
-        </form>
+    <div className="bg-slate-200 w-[500px] m-auto mt-[210px] rounded-xl">
+      <h1 className="text-center pt-10 text-[24px] uppercase">Авторизация</h1>
+      <form className="flex flex-col" onSubmit={hendleSignUp}>
+        <input
+          className="w-[260px] mt-10 m-auto  h-[40px] rounded-full pl-3 focus:outline-none focus:ring-1 focus:ring-orange-400"
+          placeholder="login"
+          onChange={handleLogin}
+          value={login}
+          type="text"
+        />
+        <input
+          className="w-[260px] mt-5 m-auto focus:outline-none focus:ring-1 focus:ring-orange-400 h-[40px] rounded-full pl-3"
+          placeholder="passsword"
+          onChange={handlePassword}
+          value={password}
+          type="password"
+        />
+        <button
+          className=" w-[173px] uppercase h-[40px] text-[#f9961e] m-auto my-10 bg-[#fff] rounded-full text-sm hover:bg-[#f9961e] hover:text-white hover:border-orange-400 hover:border border border-orange-400"
+          type="submit"
+        >
+          Войти
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
