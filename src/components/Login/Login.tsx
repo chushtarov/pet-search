a8040b193d9edab9e76cd900995496f006524c92
chushtarov <kenlol@bk.ru>
2024-03-06 15:21:12 +0300

    добавил стили

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -16,20 +16,26 @@ const hendleSignUp = (e: FormEvent) => {
         console.log(login, password)
     }
   return (
-    <div className='bg-[#928f8f]'>
-        <h1>Login</h1>
-        <form onSubmit={hendleSignUp}>
+    <div className='bg-slate-100 w-[500px] m-auto mt-[210px] rounded-xl'>
+        <h1 className='text-center pt-10 text-[24px] uppercase'>Авторизация</h1>
+        <form 
+        className='flex flex-col' 
+        onSubmit={hendleSignUp}>
             <input 
+            className='w-[260px] mt-10 m-auto  h-[40px] rounded-full pl-3 focus:outline-none focus:ring-1 focus:ring-orange-400'
             placeholder='login'
             onChange={handleLogin}
             value={login}
             type="text" />
             <input 
+            className='w-[260px] mt-5 m-auto focus:outline-none focus:ring-1 focus:ring-orange-400 h-[40px] rounded-full pl-3'
             placeholder='passsword'
             onChange={handlePassword}
             value={password}
             type="password" />
-            <button type='submit'>Войти</button>
+            <button 
+            className=" w-[173px] uppercase h-[40px] text-[#f9961e] m-auto my-10 bg-[#fff] rounded-full text-sm hover:bg-[#f9961e] hover:text-white hover:border-orange-400 hover:border border border-orange-400"
+            type='submit'>Войти</button>
         </form>
     </div>
   )
